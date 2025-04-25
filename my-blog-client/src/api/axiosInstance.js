1865284455c8d1671d2eb4e6420000ac7d6689e7
CheolWan <sam_000@naver.com>
2025-04-25 18:27:24 +0900

    baseURL 수정

--- my-blog-client/src/api/axiosInstance.js
+++ my-blog-client/src/api/axiosInstance.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: process.env.NEXT_PUBLIC_API_URL, // 환경변수에서 처리
 });
 
 // 요청 보낼 때마다 localStorage에서 토큰 자동으로 읽어와 헤더에 추가
 instance.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
